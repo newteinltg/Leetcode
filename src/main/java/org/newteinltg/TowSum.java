dbce312447b675d8d57fb3a70a07d42913f6da7a
newteinltg <newteinltg@163.com>
2023-08-30 15:09:05 +0800

    1、twoSum

---
+++ src/main/java/org/newteinltg/TowSum.java
@@ -0,0 +1,61 @@
+package org.newteinltg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *
+ */
+
+ /**
+ * 使用两层for循环进行遍历求解
+ */
+public class TowSum {
+    public int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = (i + 1); j < length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * 使用HashMap进行程序编写
+     *
+     * @param nums   int[]
+     * @param target int
+     * @return int[]
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i])) {
+                return new int[]{numsMap.get(target - nums[i]), i};
+            }
+            numsMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
